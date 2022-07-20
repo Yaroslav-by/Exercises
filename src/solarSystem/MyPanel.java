c1d273f08f09a473318357da4feb679f42c6235d
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-20 21:38:04 +0300

    The Earth is done

--- src/solarSystem/MyPanel.java
+++ src/solarSystem/MyPanel.java
@@ -12,6 +12,7 @@ public class MyPanel extends JPanel {
 	private int y = (int) (10 * Math.sin(Math.toRadians(0)));
 	private BufferedImage imageSun;
 	private BufferedImage imageEarth;
+	private int count = 1;
 	
 	MyPanel() {
 		
@@ -24,6 +25,9 @@ public class MyPanel extends JPanel {
 			e.printStackTrace();
 		}
 		
+		Timer timer = new Timer(10, (e) -> newCoord());
+		timer.start();
+		
 		System.out.println(x + " " + y);
 		
 	}
@@ -34,8 +38,17 @@ public class MyPanel extends JPanel {
 		g2D.setBackground(Color.darkGray);
 		g2D.clearRect(0, 0, 700, 700);
 		g2D.drawImage(imageSun, 0, 0, this);
-		g2D.drawImage(imageEarth, 400, 400, this);
+		g2D.drawImage(imageEarth, 310 + this.x, 310 + this.y, this);
 		
+		
+	}
+	
+	public void newCoord() {
+		
+		this.x = (int) (150 * Math.cos(Math.toRadians(count)));
+		this.y = (int) (150 * Math.sin(Math.toRadians(count)));
+		count++;
+		repaint();
 		
 	}
 }

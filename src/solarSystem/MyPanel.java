8e9fc96d84353a1966ed4592c2f53c81d1b4715e
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-21 19:07:35 +0300

    Planets are done

--- src/solarSystem/MyPanel.java
+++ src/solarSystem/MyPanel.java
@@ -8,15 +8,21 @@ import javax.swing.*;
 
 public class MyPanel extends JPanel {
 
-	private int x = (int) (150 * Math.cos(Math.toRadians(0)));
-	private int y = (int) (150 * Math.sin(Math.toRadians(0)));
+	private int x = (int) (120 * Math.cos(Math.toRadians(0)));
+	private int y = (int) (120 * Math.sin(Math.toRadians(0)));
 	private int x1 = (int) (300 * Math.cos(Math.toRadians(0)));
 	private int y1 = (int) (300 * Math.sin(Math.toRadians(0)));
+	private int x2 = (int) (200 * Math.cos(Math.toRadians(0)));
+	private int y2 = (int) (200 * Math.sin(Math.toRadians(0)));
+	
 	private BufferedImage imageSun;
 	private BufferedImage imageEarth;
-	private BufferedImage imageEarth1;
+	private BufferedImage imageSaturn;
+	private BufferedImage imageUranus;
+	
 	private int count = 1;
 	private double count1 = 0.5;
+	private double count2 = 0.7;
 	
 	MyPanel() {
 		
@@ -25,7 +31,8 @@ public class MyPanel extends JPanel {
 		try {
 			imageSun = ImageIO.read(new File("Images\\Sun.png"));
 			imageEarth = ImageIO.read(new File("Images\\Earth.png"));
-			imageEarth1 = ImageIO.read(new File("Images\\Earth.png"));
+			imageSaturn = ImageIO.read(new File("Images\\Saturn.png"));
+			imageUranus = ImageIO.read(new File("Images\\Uranus.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -42,18 +49,22 @@ public class MyPanel extends JPanel {
 		g2D.clearRect(0, 0, 700, 700);
 		g2D.drawImage(imageSun, 0, 0, this);
 		g2D.drawImage(imageEarth, 310 + this.x, 310 + this.y, this);
-		g2D.drawImage(imageEarth1, 310 + this.x1, 310 + this.y1, this);
+		g2D.drawImage(imageUranus, 310 + this.x1, 310 + this.y1, this);
+		g2D.drawImage(imageSaturn, 310 + this.x2, 310 + this.y2, this);
 		
 	}
 	
 	public void newCoordEarth() {
 		
-		this.x = (int) (150 * Math.cos(Math.toRadians(count)));
-		this.y = (int) (150 * Math.sin(Math.toRadians(count)));
+		this.x = (int) (120 * Math.cos(Math.toRadians(count)));
+		this.y = (int) (120 * Math.sin(Math.toRadians(count)));
 		this.x1 = (int) (300 * Math.cos(Math.toRadians(count1)));
 		this.y1 = (int) (300 * Math.sin(Math.toRadians(count1)));
-		count++;
-		count1 += 0.5;
+		this.x2 = (int) (200 * Math.cos(Math.toRadians(count2)));
+		this.y2 = (int) (200 * Math.sin(Math.toRadians(count2)));
+		count += 1.3;
+		count1 += 0.3;
+		count2 += 0.5;
 		repaint();
 		
 	}

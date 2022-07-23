54f693143dd645d6a8bd4e91bffb1af7ff838393
Yaroslav <asgen-yaroslav@mail.ru>
2022-07-23 21:05:55 +0300

    Grass is done

--- src/fillWithTexture/MyPanel.java
+++ src/fillWithTexture/MyPanel.java
@@ -1,7 +1,10 @@
 package fillWithTexture;
 
 import java.awt.Dimension;
-import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.TexturePaint;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,19 +14,29 @@ import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
 	
-	BufferedImage image;
+	TexturePaint p;
 
 	MyPanel() {
 		
 		this.setPreferredSize(new Dimension(800, 600));
-		
+	
 		try {
-			image = ImageIO.read(new File("Images//GrassTexture.jpg"));
+			BufferedImage i = ImageIO.read(new File("Images//GrassTexture.jpg"));
+			p = new TexturePaint(i, new Rectangle(50, 50));
 		} catch (IOException e) {
+
 			e.printStackTrace();
 		}
 		
-		System.out.println(image.getWidth() + " " + image.getHeight());
+	}
+	
+	public void paint(Graphics g) {
+		
+		Graphics2D g2D = (Graphics2D) g;
+		
+		g2D.setPaint(p);
+		g2D.fillRect(0, 0, 800, 600);
+		
 	}
 
 }

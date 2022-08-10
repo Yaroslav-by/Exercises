e96b18d8667a3e0a3edb42e732dade85983c8a54
Yaroslav <asgen-yaroslav@mail.ru>
2022-08-10 22:42:22 +0300

    Favorite season is added

---
+++ src/jjjRadioButton/Main.java
@@ -0,0 +1,11 @@
+package jjjRadioButton;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MyFrame frame = new MyFrame();
+
+	}
+	
+}
---
+++ src/jjjRadioButton/MyFrame.java
@@ -0,0 +1,17 @@
+package jjjRadioButton;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+	public MyFrame() {
+		
+		MyPanel panel = new MyPanel();
+		this.setSize(800, 600);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(panel);
+		this.pack();
+		this.setVisible(true);
+		
+	}
+}
